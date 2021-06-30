6e964a99f1a471ecc3c090091105193ab475e47f
Himanshu Shekhar <42701850+battleplayer02@users.noreply.github.com>
2021-06-30 16:47:28 +0530

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,13 @@
+import { useState } from "react"
 
 const Header = () => {
+
+    const [toggle, settoggle] = useState<boolean>(false)
+
+    function toggleMenu() {
+        settoggle(!toggle)
+    }
+
     return (
         <nav className="bg-gray-800">
             <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
@@ -9,40 +17,10 @@ const Header = () => {
                             <h6 className="p-1 bg-gray-100 rounded">Twrus Tech Shop</h6>
                         </div>
                         <div className="hidden md:block">
-                            <div className="flex items-baseline ml-10 space-x-4">
+                            <div className="flex ml-10 space-x-4">
                                 <a
-                                    href="#"
-                                    className="px-3 py-2 text-sm font-medium text-white rounded-md hover:bg-gray-700"
-                                >
+                                    className="px-3 py-2 text-sm font-medium text-white rounded-md hover:bg-gray-700">
                                     Dashboard
-                                </a>
-
-                                <a
-                                    href="#"
-                                    className="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
-                                >
-                                    Team
-                                </a>
-
-                                <a
-                                    href="#"
-                                    className="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
-                                >
-                                    Projects
-                                </a>
-
-                                <a
-                                    href="#"
-                                    className="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
-                                >
-                                    Calendar
-                                </a>
-
-                                <a
-                                    href="#"
-                                    className="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
-                                >
-                                    Reports
                                 </a>
                             </div>
                         </div>
@@ -50,11 +28,11 @@ const Header = () => {
                     <div className="flex -mr-2 md:hidden">
                         <button
                             type="button"
-                            className="inline-flex items-center justify-center p-2 text-gray-400 bg-gray-800 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                            className="inline-flex items-center justify-center p-2 text-gray-400 bg-gray-800 rounded-md hover:text-white hover:bg-gray-700 "
                             aria-controls="mobile-menu"
                             aria-expanded="false"
+                            onClick={toggleMenu}
                         >
-                            <span className="sr-only">Open main menu</span>
 
                             <svg
                                 className="block w-6 h-6"
@@ -71,65 +49,19 @@ const Header = () => {
                                     d="M4 6h16M4 12h16M4 18h16"
                                 />
                             </svg>
-
-                            <svg
-                                className="hidden w-6 h-6"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                aria-hidden="true"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M6 18L18 6M6 6l12 12"
-                                />
-                            </svg>
                         </button>
                     </div>
                 </div>
             </div>
-
-            <div className="md:hidden" id="mobile-menu">
+            {toggle && <div className="md:hidden">
                 <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
                     <a
-                        href="#"
-                        className="block px-3 py-2 text-base font-medium text-white rounded-md hover:bg-gray-700"
-                    >
+                        className="block px-3 py-2 font-medium text-white rounded-md hover:bg-gray-700">
                         Dashboard
-                    </a>
-
-                    <a
-                        href="#"
-                        className="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
-                    >
-                        Team
-                    </a>
-
-                    <a
-                        href="#"
-                        className="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
-                    >
-                        Projects
-                    </a>
-
-                    <a
-                        href="#"
-                        className="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
-                    >
-                        Calendar
-                    </a>
-
-                    <a
-                        href="#"
-                        className="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
-                    >
-                        Reports
                     </a>
                 </div>
             </div>
+            }
         </nav>
     )
 }
