e240e8d99e300058423d68fc45e728923f0a5509
Himanshu Shekhar <42701850+battleplayer02@users.noreply.github.com>
2021-06-30 12:52:14 +0530

    added tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 
 export default function Home() {
   return (
-    <button className="px-4 py-2 font-semibold text-white bg-green-500 rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75">
-      Hello
-    </button>
+    <>
+      <h1 className="text-green-400">Hello</h1>
+      <button className="px-4 py-2 font-semibold text-white bg-green-500 rounded-lg shadow-md hover:bg-green-700">
+        Click me
+      </button>
+    </>
   )
 }
